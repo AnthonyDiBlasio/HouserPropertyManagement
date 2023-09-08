e4c8c7ddeca890c687831ece8617b10d26ef09cb
Anthony DiBlasio <anthony.diblasio11@gmail.com>
2023-09-08 19:39:02 -0400

    updating css, and home.js

--- src/Home.js
+++ src/Home.js
@@ -3,7 +3,7 @@ import Mower from './images/noun-lawn-mower-197583.png'
 import Trim from './images/noun-trimming-grass-2147133.png'
 import Mulch from './images/noun-mulching-4474764.png'
 import Snow from './images/noun-snow-plow-4946438.png'
-import Commercial from './images/noun-lawn-trimming-2146829.png'
+import Hardscaping from './images/noun-construction-2085884.png'
 import Tree from './images/noun-tree-trimming-2057492.png'
 import photo1 from './images/photo1.jpg'
 import photo2 from './images/photo2.jpg'
@@ -17,41 +17,47 @@ import photo9 from './images/photo9.jpg'
 import photo10 from './images/photo10.jpg'
 import photo11 from './images/photo11.jpg'
 
+
 import logo from './images/houserlogo-removebg-preview.png'
 
 
 function Home() {
     return (
-        <div className='container'>
-            <section className='header'>
-                <div >
-                    <img className='ImgOverlay'style={{ marginTop: "5pc" }} src={logo} alt='imgoverlay'></img>
+         /* Site container*/
+        <div id='root'className=''>
+            <section className='row first-row'>
+                <div className='header'>
+                    <img className='ImgOverlay'style={{ marginTop: "5pc", marginLeft:"5pc"}} src={logo} alt='imgoverlay'></img>
                     <div className='text-center'>Rooted In Quality, Growing in Trust
-                    <p>Houser Property Management has been servicing the greater Bucks County area for over 7 years. The team</p>
+                    <p>Houser Property Management has been servicing the greater Bucks County area for over 7 years.</p>
                     </div>
                 </div>
             </section>
-         <section style={{backgroundColor: ""}} className='row'>
+        <div className='container'>
+         <section className='row'>
                 <div style={{marginBottom:"5pc"}} className='text-center services'>Services Offered</div>
                 <div className='col col-2'>Landscaping
-                    <img style={{ height: "100px" }} src={Mower} alt='mow'></img>
+                    <img style={{ width: "100px" }} src={Mower} alt='mow'></img>
                 </div>
                 <div className='col col-2'>Hardscaping
-                    <img style={{ height: "100px" }} src={Commercial} alt='com'></img>
+                    <img style={{width: "100px" }} src={Hardscaping} alt='com'></img>
                 </div>
                 <div className='col col-2'>Tree Care
-                    <img style={{ height: "100px" }} src={Tree} alt='tree'></img>
+                    <img style={{ width: "100px" }} src={Tree} alt='tree'></img>
                 </div>
                 <div className='col col-2'>Mulching
-                    <img style={{ height: "100px" }} src={Mulch} alt='mdivch'></img>
+                    <img style={{ width: "100px" }} src={Mulch} alt='mdivch'></img>
                 </div>
-                <div className='col-2'>Pruning/Trimming
-                    <img style={{height: "100px" }} src={Trim} alt='trim'></img>
+                <div className='col col-2'>Pruning/Trimming
+                    <img style={{width: "100px" }} src={Trim} alt='trim'></img>
                 </div>
                 <div className='col col-2'>Snow Removal
-                    <img style={{ height: "100px" }} src={Snow} alt='snow'></img>
+                    <img style={{ width: "100px" }} src={Snow} alt='snow'></img>
                 </div>
                 <div className='text-center call'>Call (215)-470-5755 Today For a Free Qoute</div>
+                
+            </section>
+            
                 <section className='PhotoGroup'>
                 <Carousel data-bs-theme="dark">
                     <Carousel.Item>
@@ -137,9 +143,8 @@ function Home() {
                     </Carousel.Item>
                 </Carousel>
             </section>
-
-            </section>
-         
+            </div>
+      
         </div>
     );
 }
